84cc92a914e539a853163d667eafa17d8ef02104
shawat94 <shawa947@gmail.com>
2022-04-24 13:06:23 -0700

    uibuild

--- app.js
+++ app.js
@@ -28,4 +28,8 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+})
+
 module.exports = app
