fb206fda83ca795470a8d3c230a421affd02bd9a
shawat94 <shawa947@gmail.com>
2022-04-24 19:50:26 -0700

    uibuild

--- app.js
+++ app.js
@@ -24,9 +24,17 @@ app.use('/api/login', loginRouter)
 
 app.use(express.static('build'))
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
